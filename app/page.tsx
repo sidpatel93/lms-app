5b2479f63a2083992944957c45026c11ee765c35
Sid Patel <sid.auburn@gmail.com>
2024-10-05 17:12:10 -0400

    learning how to create page route in the next

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <p className="text-3xl font-medium text-sky-700">hello World</p>
-      <Button variant="link">Click me</Button>
+      <Button>Click me</Button>
     </div>
   );
 }
