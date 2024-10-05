bc9e0a82bf42237f581b2ef807cf3053c73a94df
Sid Patel <sid.auburn@gmail.com>
2024-10-05 18:01:06 -0400

    resolved the issue with page hirarchy

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  auth,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
@@ -7,7 +11,6 @@ export default clerkMiddleware((auth, request) => {
     auth().protect();
   }
 });
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
