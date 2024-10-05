c293cc35af69d3a62ccbcc31771a56582a7115af
Sid Patel <sid.auburn@gmail.com>
2024-10-05 17:46:15 -0400

    implemented the clerk auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-medium text-sky-700">
-        Hello this is protected page.
-      </p>
+      <UserButton />
     </div>
   );
 }
